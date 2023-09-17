72f3c1f0f4160b50843a4da5a7e8fe45d39b0b42
arjunkmrm <arjunkmrm@gmail.com>
2023-09-17 13:38:18 +0800

    update flags

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy, os, sys
 
+import os
+import sys
+
 if sys.platform == 'darwin':
-    os.environ['CFLAGS']   = '-DGGML_USE_ACCELERATE -O3 -std=gnu11'
-    os.environ['CXXFLAGS'] = '-DGGML_USE_ACCELERATE -O3 -std=c++11'
-    os.environ['LDFLAGS']  = '-framework Accelerate'
+    os.environ['CFLAGS']   = '-DGGML_USE_ACCELERATE -O3 -std=gnu11 -I/opt/homebrew/Cellar/sdl2/2.28.3/include/SDL2'
+    os.environ['CXXFLAGS'] = '-DGGML_USE_ACCELERATE -O3 -std=c++11 -I/opt/homebrew/Cellar/sdl2/2.28.3/include/SDL2'
+    os.environ['LDFLAGS']  = '-framework Accelerate -L/opt/homebrew/Cellar/sdl2/2.28.3/lib'
 else:
-    os.environ['CFLAGS']   = '-mavx -mavx2 -mfma -mf16c -O3 -std=gnu11'
-    os.environ['CXXFLAGS'] = '-mavx -mavx2 -mfma -mf16c -O3 -std=c++11'
+    os.environ['CFLAGS']   = '-mavx -mavx2 -mfma -mf16c -O3 -std=gnu11 -I/opt/homebrew/Cellar/sdl2/2.28.3/include/SDL2'
+    os.environ['CXXFLAGS'] = '-mavx -mavx2 -mfma -mf16c -O3 -std=c++11 -I/opt/homebrew/Cellar/sdl2/2.28.3/include/SDL2'
+    os.environ['LDFLAGS']  = '-L/opt/homebrew/Cellar/sdl2/2.28.3/lib -lSDL2'
 
 ext_modules = [
     Extension(
@@ -17,7 +21,7 @@ ext_modules = [
         sources=["whispercpp.pyx", "whisper.cpp/whisper.cpp", "stream.cpp"],
         language="c++",
         extra_compile_args=["-std=c++11"],
-        include_dirs=['/opt/homebrew/opt/sdl2']        
+        include_dirs=['/opt/homebrew/Cellar/sdl2/2.28.3/include/SDL2/']        
    )
 ]
 ext_modules = cythonize(ext_modules)
@@ -32,7 +36,7 @@ setup(
     author_email='luke@devthe.com',
     libraries=[whisper_clib],
     ext_modules = cythonize("whispercpp.pyx"),
-    include_dirs = ['./whisper.cpp/', '/opt/homebrew/opt/sdl2', numpy.get_include()],
+    include_dirs = ['./whisper.cpp/', '/opt/homebrew/Cellar/sdl2/2.28.3/include/SDL2/', numpy.get_include()],
     install_requires=[
       'numpy',
       'ffmpeg-python',
